c78efa264b2ecb773a8045058b915a7f433860d3
hackarchives <admin@undergroundlegacy.co.cc>
2013-07-10 16:29:36 +0530

    Error Handling Added

--- post_handler/getservinfo.php
+++ post_handler/getservinfo.php
@@ -1,21 +1,31 @@
 <?php
 if(isset($_POST['url']))
 {
-  $url = $_POST['url'];
+	$url = $_POST['url'];
 	$id = $_POST['pid'];
 	$headers = get_headers($url, 1);
 	include('../db.php');
 	$date = date("d-m-Y");
 	$time = date("H:i:s");
-	if(array_key_exists('X-Powered-By', $headers)) 
+	if(array_key_exists('X-Powered-By', $headers) &&  array_key_exists('Server',$headers)) 
 	{
 		$q = $db->prepare("INSERT INTO scaninfo (pid,date,time,server,lang,content) VALUES (?,?,?,?,?,?)");
 		$q->execute(array($id,$date,$time,$headers["Server"],$headers["X-Powered-By"],$headers["Content-Type"]));
 	}
-	else
+	else if(!array_key_exists('X-Powered-By', $headers) &&  array_key_exists('Server',$headers))
 	{
 		$q = $db->prepare("INSERT INTO scaninfo (pid,date,time,server,lang,content) VALUES (?,?,?,?,?,?)");
 		$q->execute(array($id,$date,$time,$headers["Server"],"NULL",$headers["Content-Type"]));		
+	}
+	else if(array_key_exists('X-Powered-By', $headers) &&  !array_key_exists('Server',$headers))
+	{
+		$q = $db->prepare("INSERT INTO scaninfo (pid,date,time,server,lang,content) VALUES (?,?,?,?,?,?)");
+		$q->execute(array($id,$date,$time,"NULL",$headers["X-Powered-By"],$headers["Content-Type"]));		
+	}
+	else if(!array_key_exists('X-Powered-By', $headers) &&  !array_key_exists('Server',$headers))
+	{
+		$q = $db->prepare("INSERT INTO scaninfo (pid,date,time,server,lang,content) VALUES (?,?,?,?,?,?)");
+		$q->execute(array($id,$date,$time,"NULL","NULL",$headers["Content-Type"]));		
 	}
 	$t = json_encode($headers);
 	echo $t;
