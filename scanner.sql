ef8c6dbdf5a303dc1398da43ece2ee61402c7820
hackarchives <admin@undergroundlegacy.co.cc>
2013-07-10 11:59:30 +0530

    Added Another column to get savepoint information

--- scanner.sql
+++ scanner.sql
@@ -1,42 +1,72 @@
-SET FOREIGN_KEY_CHECKS=0;
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 10, 2013 at 06:28 AM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.4.3
+
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
-CREATE DATABASE scanner DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE scanner;
+--
+-- Database: `scanner`
+--
+CREATE DATABASE `scanner` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `scanner`;
 
-CREATE TABLE scan (
-  id int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scan`
+--
+
+CREATE TABLE IF NOT EXISTS `scan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(300) NOT NULL,
-  url varchar(1000) NOT NULL,
-  PRIMARY KEY (id)
+  `url` varchar(1000) NOT NULL,
+  `started` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-CREATE TABLE scaninfo (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  pid int(11) NOT NULL,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scaninfo`
+--
+
+CREATE TABLE IF NOT EXISTS `scaninfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL,
   `date` varchar(50) NOT NULL,
   `time` varchar(50) NOT NULL,
   `server` varchar(255) NOT NULL,
-  lang varchar(255) NOT NULL,
-  content varchar(255) NOT NULL,
-  PRIMARY KEY (id)
+  `lang` varchar(255) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-CREATE TABLE spider_url (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  scanid int(11) NOT NULL,
-  url varchar(1000) NOT NULL,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spider_url`
+--
+
+CREATE TABLE IF NOT EXISTS `spider_url` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `scanid` int(11) NOT NULL,
+  `url` varchar(1000) NOT NULL,
   `status` int(11) NOT NULL,
-  pid int(11) NOT NULL,
-  PRIMARY KEY (id)
+  `pid` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-SET FOREIGN_KEY_CHECKS=1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
